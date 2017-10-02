fbf06180d52bae76760829d131acbf38a719c83e
Aliaksandr Talanau <aliaksandr_talanau@epam.com>
2017-10-02 18:23:52 +0300

    HW2 update

--- dirwatcher/index.js
+++ dirwatcher/index.js
@@ -1,41 +1,25 @@
-import * as fs from "fs";
 import * as EventEmitter from "events";
-
+import * as chokidar from "chokidar";
 
 export class DirWatcher extends EventEmitter.EventEmitter{
-    filesCount;
-    filesList;
     constructor(){
         super();
-        this.filesCount=0;
-        this.filesList=[];
     }
     
     watch(path, delay){
-        var that = this;
-        setInterval(this.readDirectory, 1000, path, function(err, data){
-            if (err) {
-                throw err;
-            }
-            if(data.length>that.filesCount){
-                console.log("Output:");
-                data.forEach(function(item,i,arr){
-                    if(that.filesList.indexOf(item)==-1)
-                        that.emit("dirwatcher:changed", path+"/"+item);
-                });
-            }
-            else{
-                console.log("No changes");
-            }
-            that.filesCount=data.length;
-            that.filesList=data;
+        var watcher = chokidar.watch(path, {
+            ignored: /(^|[\/\\])\../,
+            persistent: true,
+            interval: delay
+        });
+        watcher.on("add", function(){
+            this.emit("dirwatcher:changed");
+        });
+        watcher.on("change", function(){
+            this.emit("dirwatcher:changed");
         });
     }
-    readDirectory(path, callback){
-        fs.readdir(path, callback);
-    }
-    subscribe(event, listener){
-        
+    subscribe(listener){
         this.addListener("dirwatcher:changed",listener)
     }
 }
